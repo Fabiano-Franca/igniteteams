d73f8eef7e5ee436e48f922bc63a67600001c141
Fabiano-Franca <fabianofranca.ti@gmail.com>
2023-10-07 19:50:26 -0300

    Finalizado segunda tela

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,8 +1,14 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { useState } from 'react';
+import { Button, FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header />
@@ -10,8 +16,22 @@ export function Groups() {
         title='Turmas'
         subtitle='jogue com a sua turma'
       />
-    </Container>
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?" />}
+      />
 
+      <Button
+        title='Criar nova turma'
+      />
+    </Container>
   );
 }
 
